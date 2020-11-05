9ceee822df95b288461b0af9066921394d3acbf9
Praneeth <bpraneeth2000@gmail.com>
2020-11-05 15:31:51 +0530

    m

--- week 5/fibonacci.sh
+++ week 5/fibonacci.sh
@@ -7,10 +7,11 @@ prev=0
 i=1
 echo "The fibonacci series upto $n is"
 printf "$prev "
-while [ $i -le $n ]
+while [ $n -ge 2 ]
 do
 printf "$i "
 temp=$i
+n=`expr $n - 1`
 i=`expr $i + $prev`
 prev=$temp
 done
